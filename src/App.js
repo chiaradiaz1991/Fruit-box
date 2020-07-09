456ceccb67e0b7ff5e5724fd6b642f419645abd6
chiaradiaz1991 <chiara.diaz1991@gmail.com>
2020-07-09 15:01:32 +0200

    added some styles and show the fruits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
+import './App.css'
 
 
 @inject('FruitStore') //the same as the provider, our store
@@ -16,13 +17,20 @@ class App extends Component {
   render() {
     const { FruitStore } = this.props;
     return (
-      <div className="App">
+      <div className="app-container">
         {/* as it's a get, we write the function without (), we access it as it's a prop */}
         <h2>You have { FruitStore.fruitCount } fruits!</h2>
         <form onSubmit={e => this.handleSubmit(e)}>
           <input type="text" placeholder="enter a fruit..." ref={input => this.fruit = input} />
           <button>Add a fruit</button>
         </form>
+
+        <ul>
+          { FruitStore.fruits.map((fruit, index) => (
+            <li key={index}>🍊 {fruit}</li>
+          ))
+          }
+        </ul>
       </div>
     );
   }
